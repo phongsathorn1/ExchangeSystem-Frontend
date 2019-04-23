0ca37cc469b1847714af45bf685a6db200757e64
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-23 22:25:38 +0700

    Import vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
