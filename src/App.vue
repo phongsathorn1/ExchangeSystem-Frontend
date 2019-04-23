be5bc85a0db285750c8aa3063675163497062e0e
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-23 22:26:29 +0700

    Change style

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+
+<style lang="scss">
+@import "assets/custom.scss";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  background: #eceff1;
+  min-height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   css: {
+//     loaderOptions: {
+//       less: {
+//         modifyVars: {
+//           'font-family': '\'Avenir\', Helvetica, Arial, sans-serif'
+//         },
+//         javascriptEnabled: true
+//       }
+//     }
+//   }
+// }
